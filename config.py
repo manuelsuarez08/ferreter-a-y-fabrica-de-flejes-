fc46f039791382a8f1dc9bfe0c76ba6a10b9400b
manuel stiben suarez narvaez <manuelsuarezn08@gmail.com>
2026-08-29 12:05:43 -0500

    actualizacion de configuracion

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Configuración de OpenAI
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY =
 
 # Otras configuraciones
 DEBUG = True
